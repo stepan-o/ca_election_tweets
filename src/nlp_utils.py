8a4d1874ecc6d2b07de96146431658126f2b567d
Stepan Oskin <stepanoskin007@gmail.com>
2019-10-15 23:46:06 -0400

    add removal of non-ASCII characters to preprocessor

--- src/nlp_utils.py
+++ src/nlp_utils.py
@@ -13,6 +13,8 @@ def preprocessor(text):
     emoticons = re.findall('[:;=](?:-)?[)(DP|]', text)
     # lower case, remove all non-word characters, add emoticons to the end of the string, remove the "nose" '-'
     text = (re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', ''))
+    # remove non-English unicode characters (not from the ASCII character set)
+    text = re.sub('[^\x00-\x7F]', '', text)
     # strip leading and trailing whitespaces, replace multiple spaces with a single
     text = re.sub('\s+', ' ', text)
     return text
